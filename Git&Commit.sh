2bead6d1a1fa3768c18939f313a79c2f091fcfb0
Joshua Hanna <2wvqr47798@privaterelay.appleid.com>
2026-03-08 07:45:21 -0500

    Create Git&Commit.sh
    BaH

---
+++ Git&Commit.sh
@@ -0,0 +1,10 @@
+cd ~/Projects/MacKeyboardiOS
+
+# Add all files
+git add .
+
+# Create initial commit
+git commit -m "Initial commit: macOS keyboard layout for iOS with hotkeys and special keys"
+
+# View git log
+git log --oneline
